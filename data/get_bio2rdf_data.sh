531f8edd417009e97882cd35bbae8e117fec93ed
Michel Dumontier <michel.dumontier@gmail.com>
2023-09-20 16:24:45 -0400

    refactoring the project directories

--- data/get_bio2rdf_data.sh
+++
@@ -1,3 +0,0 @@
-curl -F 'format=text/csv' -F "query=@drug_targets.rq" "https://bio2rdf.org/sparql" > drug_targets.csv
-curl -F 'format=text/csv' -F "query=@drugs.rq" "https://bio2rdf.org/sparql" > drugs.csv
-curl -F 'format=text/csv' -F "query=@targets.rq" "https://bio2rdf.org/sparql" > targets.csv
---
+++ run_esm.sh
@@ -0,0 +1,1 @@
+esm-extract esm2_t33_650M_UR50D data/download/drugbank_targets.fasta data/vectors/drugbank_targets_esm2_l33_mean --repr_layers 33 --include mean
