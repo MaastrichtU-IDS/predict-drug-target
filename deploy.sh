1b40c9f215b3d29fc689270deb13d06fe1104486
Vincent Emonet <vincent.emonet@gmail.com>
2023-10-17 12:41:16 +0200

    fix compose

--- deploy.sh
+++ deploy.sh
@@ -2,8 +2,8 @@
 
 if [ "$1" = "--no-cache" ]; then
     echo "📦️ Building without cache"
-    ssh ids2 'cd /mnt/um-share-drive/vemonet/predict-drug-target ; git pull ; docker-compose build --no-cache ; docker-compose -f docker-compose.yml -f docker-compose.prod.yml down ; docker-compose -f docker-compose.yml -f docker-compose.prod.yml up --force-recreate -d'
+    ssh idsg1 'cd /mnt/um-share-drive/vemonet/predict-drug-target ; git pull ; docker compose build --no-cache ; docker-compose -f docker-compose.yml -f docker-compose.prod.yml down ; docker-compose -f docker-compose.yml -f docker-compose.prod.yml up --force-recreate -d'
 else
     echo "♻️ Building with cache"
-    ssh idsg1 'cd /mnt/um-share-drive/vemonet/predict-drug-target ; git pull ; docker-compose up --force-recreate --build -d'
+    ssh idsg1 'cd /mnt/um-share-drive/vemonet/predict-drug-target ; git pull ; docker compose up --force-recreate --build -d'
 fi
